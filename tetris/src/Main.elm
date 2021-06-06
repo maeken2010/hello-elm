a3423dcac7feb10d59c250191ef1bb11b811bd05
maeken <maeken2010@gmail.com>
2021-06-06 20:25:08 +0900

    ミノが積まれるように

--- tetris/src/Main.elm
+++ tetris/src/Main.elm
@@ -7,6 +7,10 @@ import Html.Events exposing (onClick)
 
 import Set exposing (Set, fromList, member, empty, union, map)
 import List exposing (map, range)
+import Html exposing (a)
+
+debugWithToString : a -> a
+debugWithToString a = Debug.log (Debug.toString a) a
 
 boardSizeX = 10
 boardSizeY = 20
@@ -19,20 +23,41 @@ type Msg = Click
 -- model
 type alias Coordinate = (Int, Int)
 type alias CellSet = Set Coordinate
-type alias Model = { mino : CellSet, block : CellSet }
+type alias Mino = Set Coordinate
+
+type alias Model = { mino : Mino, block : CellSet }
 init : Model
 init = initBoard
 
 minoT : CellSet
 minoT = fromList [(1,1), (2,1), (3,1), (2,2)]
 
+bottomBlock : CellSet
+bottomBlock = List.range 1 boardSizeX
+    |> List.map (\x -> (x, boardSizeY + 1))
+    |> Set.fromList
+
 initBoard : Model
-initBoard = {mino = minoT, block = empty}
+initBoard = {mino = minoT, block = bottomBlock}
 
 -- update
 update : Msg -> Model -> Model
 update msg model = case msg of
-   Click -> {block = model.block, mino = (Set.map (\(x, y) -> (x + 1, y)) model.mino)}
+    Click -> downMino model
+
+downMino : Model -> Model
+downMino model = 
+    let nextMino = (Set.map (\(x, y) -> (x, y + 1)) model.mino)
+    in if canDownMino model.block nextMino
+        then {model | mino = nextMino}
+        else {block = union model.block model.mino, mino = minoT}
+
+canDownMino : CellSet -> Mino -> Bool
+canDownMino block mino =
+    mino
+    |> Set.toList
+    |> List.filter (\a -> member a block)
+    |> List.isEmpty
 
 -- view
 cellSize : String
@@ -40,17 +65,32 @@ cellSize = "15px"
 
 view : Model -> Html Msg
 view model = div [onClick Click] [
-        div [style "border" "solid", style "width" "150px"] (List.map (\y -> viewCellRow (union model.block model.mino) y) (range 1 boardSizeY))
+        div [
+            style "display" "flex",
+            style "border" "solid",
+            style "width" "150px"
+        ] (
+            List.map (\x -> viewCellColumn (union model.block model.mino) x) (range 1 boardSizeX)
+        )
     ]
 
-viewCellRow : CellSet -> Int -> Html Msg
-viewCellRow cellSet y = div [style "display" "flex"] (List.map (\x -> cellDiv cellSet (y, x)) (range 1 boardSizeX))
+viewCellColumn : CellSet -> Int -> Html Msg
+viewCellColumn cellSet x = div [
+        style "display" "flex",
+        style "flex-direction" "column"
+    ] (
+        List.map (\y -> cellDiv cellSet (x, y)) (range 1 boardSizeY)
+    )
 
 cellDiv : CellSet -> Coordinate -> Html Msg
 cellDiv cellSet coordinate = div (cellAttribute cellSet coordinate) []
 
 cellAttribute : CellSet -> Coordinate -> List (Attribute Msg)
-cellAttribute cellSet coordinate = [style "background-color" (cellColor cellSet coordinate), style "height" cellSize, style "width" cellSize]
+cellAttribute cellSet coordinate = [
+        style "background-color" (cellColor cellSet coordinate),
+        style "height" cellSize,
+        style "width" cellSize
+    ]
 
 cellColor : CellSet -> Coordinate -> String
 cellColor cellSet coordinate = if member coordinate cellSet then "black" else "white"
